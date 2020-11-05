9ebaa55f036975198dc7e415fb271d1a67d46f00
theADAMJR <adamjf123@gmail.com>
2020-11-05 15:25:58 +0000

    test6

--- src/website/routes/blog-routes.ts
+++ src/website/routes/blog-routes.ts
@@ -37,7 +37,7 @@ router.get('/:id', async (req, res) => {
   if (!blog)
     return res.status(404).json({ code: 404, message: 'Blog not found.' });
 
-  res.render('blogs/show', { blog, parsedBody: highlightAuto(marked(blog.body)).value });
+  res.render('blogs/show', { blog, parsedBody: marked(highlightAuto(blog.body)).value });
 });
 
 router.put('/:id', validateUser, validateBlogOwner, async (req, res) => {
